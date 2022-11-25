a2b6031c06cd754f27fc2d7f8118022885eeb357
tuiovanoi <tuliovrcpa2015@gmail.com>
2022-11-25 17:58:59 -0300

    frontend

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_URL_API,
+  baseURL: 'https://metallicaweb.herokuapp.com/',
   headers: {
     'Content-Type': 'application/json',
   },
