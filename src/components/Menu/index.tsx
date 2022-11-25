c89c5f28ef11f7e604a4b990bc69da9de32338a6
tuiovanoi <tuliovrcpa2015@gmail.com>
2022-11-25 13:46:34 -0300

    frontend

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,4 +1,4 @@
-import { FcReuse } from "react-icons/fc";
+import { FcDoughnutChart } from "react-icons/fc";
 import { GrLogout } from "react-icons/gr";
 import * as S from "./styles";
 import { Link, useNavigate } from "react-router-dom";
@@ -14,7 +14,7 @@ const Menu = () => {
     <S.Cabecalho>
       <picture>
         <Link to="/">
-          <FcReuse />
+          < FcDoughnutChart/>
         </Link>
       </picture>
       <nav>
